555f6526a7d29006dc28ca0729f1c1b683e63158
Jorge <jorgedrumondjr@gmail.com>
2025-04-11 21:30:27 -0300

    adicao de um .js teste durante a aula

---
+++ codigo/js/scriptsFormularioPonto.js
@@ -0,0 +1,15 @@
+var alerta = document.querySelector(".reset")
+console.log(alerta)
+alerta.addEventListener("click", function(e){
+    e.preventDefault();
+    alert("VAMBORAAAA");
+    console.log('qualquer coisa');
+})
+
+var alerta = document.querySelector(".validacao")
+console.log(alerta)
+alerta.addEventListener("click", function(e){
+    e.preventDefault();
+    alert("VAMBORAAAA");
+    console.log('qualquer coisa');
+})
